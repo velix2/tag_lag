2909bbea15bd2d4c1f2f2c85f6da35f1265098bb
velix2 <dalix200@gmail.com>
2023-07-15 22:15:46 +0200

    removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(const TagLag());
