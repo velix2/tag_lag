8ce68e88cac27d263d5e8fb691285ab954ab1b9f
velix2 <dalix200@gmail.com>
2023-07-17 15:00:56 +0200

    Card Animation Experiment

--- lib/experimental_page.dart
+++ lib/experimental_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart';
@@ -10,12 +12,34 @@ class ExperimentalPage extends StatefulWidget {
   State<ExperimentalPage> createState() => _ExperimentalPageState();
 }
 
-class _ExperimentalPageState extends State<ExperimentalPage> {
+class _ExperimentalPageState extends State<ExperimentalPage>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation _animation;
+  AnimationStatus _status = AnimationStatus.dismissed;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 600));
+    _animation = Tween(end: 1.0, begin: 0.0).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        _status = status;
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(title: const Text("Test Page"),),
-        body: const Padding(padding: EdgeInsets.all(25),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Test Page"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
@@ -23,17 +47,40 @@ class _ExperimentalPageState extends State<ExperimentalPage> {
           children: [
             Center(
               child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Card(
-                  child: Text("Front"),
-                  
-                  ),
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 500,
+                  child: Transform(
+                      alignment: FractionalOffset.center,
+                      transform: Matrix4.identity()
+                        ..setEntry(3, 2, 0.0015)
+                        ..rotateY(pi - pi * _animation.value),
+                      child: _animation.value <= 0.5
+                          ? GestureDetector(
+                              onTap: () {
+                                  _controller.forward();
+                              },
+                              child: Card(
+                                  margin: const EdgeInsets.all(20),
+                                  child: Transform( alignment: FractionalOffset.center, transform: Matrix4.rotationY(pi), child: const Center(
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                      Icon(Icons.shuffle),
+                                      Text("Tap to pull challenge...")
+                                    ]),
+                                  ))),
+                            )
+                          : const Card(
+                              margin: EdgeInsets.all(20),
+                              child: Center(child: Text("Back")))),
+                ),
               ),
             ),
           ],
         ),
-        ),
-      
-      );
+      ),
+    );
   }
 }
