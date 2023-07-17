f7b70f414bf2f0250a391468fc2779231ecf2127
lse-git <lse.schmidteisenlohr@gmail.com>
2023-07-17 18:59:12 +0200

    started on the shop

--- lib/shop_page.dart
+++ lib/shop_page.dart
@@ -19,7 +19,53 @@ class _ShopPageState extends State<ShopPage> {
         title: const Text("Shop"),
         backgroundColor: Colors.red,
       ),
-      body: Text(appState.coinBalance.toString()),
+      body: Column(
+        children: [
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text("You have ${appState.coinBalance.toString()} Coins"),
+            )
+          ),
+          const Divider(),
+          BuyButton(
+            cost: 75,
+            medium: "U-Bahn",
+            mediumIcon: const Icon(Icons.train),
+            onPressed: () {
+              setState(() {
+                appState.coinBalance = appState.coinBalance - 75;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BuyButton extends StatefulWidget {
+  String medium;
+  int cost;
+  Icon mediumIcon;
+  var onPressed;
+
+  BuyButton(
+    {super.key, required this.cost, required this.medium, required this.mediumIcon, required this.onPressed}
+  );
+
+  @override
+  State<BuyButton> createState() => _BuyButtonState();
+}
+
+class _BuyButtonState extends State<BuyButton> {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<TagLagState>();
+    return ElevatedButton.icon(
+      onPressed: widget.onPressed,
+      label: Text("One ${widget.medium} station"),
+      icon: widget.mediumIcon,
     );
   }
 }
